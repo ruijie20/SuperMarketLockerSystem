d41d45ebcd626238ff8c499feebcac7689d49304
ruijie20 <ruijieh20@gmail.com>
2013-09-05 14:14:09 +0800

    For superRobot

---
+++ SuperMaketLockerSystemTest/SuperRobotTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using SuperMarketLockerSystem;
+
+namespace SuperMaketLockerSystemTest
+{
+    public class SuperRobotTest
+    {
+        [Test]
+        public void should_get_a_ticket_when_store_a_bag()
+        {
+            var superRobot = new SuperRobot(new List<Locker> {new Locker(2)});
+            
+            Ticket ticket = superRobot.Store(new Bag());
+            
+            Assert.NotNull(ticket);
+        }
+
+        [Test]
+        public void show_get_right_back_when_pick_with_ticket()
+        {
+            var superRobot = new SuperRobot(new List<Locker> { new Locker(2) });
+
+            var bag = new Bag();
+            Ticket ticket = superRobot.Store(bag);
+
+            Assert.AreSame(bag, superRobot.Pick(ticket));
+        }
+
+        [Test]
+        public void show_select_the_highest_vacancy_rate_locker_when_store_bag()
+        {
+            var locker1 = new Locker(3);
+            var locker2 = new Locker(2);
+            var superRobot = new SuperRobot(
+                new List<Locker>
+                    {
+                        locker1,
+                        locker2
+                    });
+
+            locker1.Store(new Bag());
+
+        }
+    }
+}
---
+++ SuperMarketLockerSystem/SuperRobot.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace SuperMarketLockerSystem
+{
+    public class SuperRobot : Robot
+    {
+        public SuperRobot(List<Locker> lockers)
+            : base(lockers)
+        {
+        }
+    }
+}
